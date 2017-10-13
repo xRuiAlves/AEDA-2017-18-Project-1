e714595e24d1ea3c1599835eae20ac65bd859dc9
xRuiAlves <up201606746@fe.up.pt>
2017-10-13 21:37:50 +0100

    Made changes to ProtecaoCivil - Now reading locais file

--- Headers/ProtecaoCivil.h
+++ Headers/ProtecaoCivil.h
@@ -13,6 +13,7 @@
 #include "IncendioDomestico.h"
 #include "IncendioFlorestal.h"
 #include "Assalto.h"
+#include "Local.h"
 #include "Erro.h"
 
 
@@ -20,17 +21,37 @@ class ProtecaoCivil {
 private:
 	std::vector<Posto*> postos;
 	std::vector<Acidente*> acidentes;
-	std::vector<std::string> locais;
+	std::vector<Local> locais;
 	const std::string ficheiroPostos;
 	const std::string ficheiroAcidentes;
 	const std::string ficheiroLocais;
 	void gravar() const;
+	unsigned int findLocal(const std::string &nomeLocal) const;
 public:
 	ProtecaoCivil(const std::string &ficheiroPostos, const std::string &ficheiroAcidentes, const std::string &ficheiroLocais);
 	~ProtecaoCivil();
 	bool addAcidente(/*TODO parametros*/);
 	bool rmAcidente(/*TODO parametros*/);
 	void openFiles();
+
+	////////////////////////////////////////////////////////
+	////////// TODO: Erase this method!!! //////////////////
+	////////////////////////////////////////////////////////
+
+	void print() const{
+		for(unsigned int i=0 ; i<postos.size() ; i++){
+			std::cout << postos.at(i)->getLocal()->getNome() << '/' << postos.at(i)->getLocal()->getXcoord() << '/' << postos.at(i)->getLocal()->getYcoord()
+					<< '/' << postos.at(i)->getNumSocorristas()<< '/' << postos.at(i)->getNumVeiculos() << std::endl;
+		}
+		std::cout << "\n\n";
+		for(unsigned int i=0 ; i<locais.size() ; i++){
+			std::cout << locais.at(i).getNome() << '/' << locais.at(i).getXcoord() << '/' << locais.at(i).getYcoord() << std::endl;
+		}
+	}
+
+	////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////
 };
 
 #endif /* PROTECAOCIVIL_H_ */
--- Source/ProtecaoCivil.cpp
+++ Source/ProtecaoCivil.cpp
@@ -6,9 +6,35 @@ ProtecaoCivil::ProtecaoCivil(const std::string &ficheiroPostos, const std::strin
 void ProtecaoCivil::openFiles(){
 	std::ifstream istr;
 
-	///////////////////////////////
-	// TODO: Ler Ficheiro Locais //
-	///////////////////////////////
+	// Ler ficheiro de locais
+	istr.open(ficheiroLocais);
+
+	if(!istr.is_open())	// ficheiro nao foi aberto com sucesso
+		throw Erro("Falha ao abrir o ficheiro \"" + ficheiroLocais + "\" no construtor de ProtecaoCivil.");
+
+	// Preencher o vetor de locais com o conteudo do ficheiro
+	std::string line, nomeLocal;
+	unsigned int x_coord, y_coord;
+	int dashIndex;
+	while(getline(istr,line)){
+		// obter nome do local
+		dashIndex = line.find_first_of('/');
+		nomeLocal = line.substr(0,dashIndex);
+		line.erase(0,dashIndex+1);
+
+		// obter a coordenada X do local
+		dashIndex = line.find_first_of('/');
+		x_coord = std::stoi(line.substr(0,dashIndex));
+		line.erase(0,dashIndex+1);
+
+		// obter a coordenada Ỹ do local
+		dashIndex = line.find_first_of('/');
+		y_coord = std::stoi(line.substr(0,dashIndex));
+
+		// acrestar este local ao vetor de locais
+		locais.push_back(Local(nomeLocal,x_coord,y_coord));
+	}
+	istr.close();	// Fechar a stream
 
 	// Ler ficheiro de postos
 	istr.open(ficheiroPostos);
@@ -17,14 +43,19 @@ void ProtecaoCivil::openFiles(){
 		throw Erro("Falha ao abrir o ficheiro \"" + ficheiroPostos + "\" no construtor de ProtecaoCivil.");
 
 	// Preencher o vetor de postos com o conteudo do ficheiro
-	std::string line, local, tipoPosto, tipoVeiculo;
-	unsigned int numSocorristas, numVeiculos, numAutotanques, numAmbulancias;
-	int dashIndex;
+	std::string tipoPosto, tipoVeiculo;
+	unsigned int numSocorristas, numVeiculos, numAutotanques, numAmbulancias, indexLocal;
 	while(getline(istr,line)){
-		// obter local
+		// obter nome do local
 		dashIndex = line.find_first_of('/');
-		local = line.substr(0,dashIndex);
+		nomeLocal = line.substr(0,dashIndex);
 		line.erase(0,dashIndex+1);
+
+		// obter indice do local no vetor
+		indexLocal = findLocal(nomeLocal);
+		if(indexLocal == -1){		// Este local nao foi encontrado no vetor de locais da protecao civil
+			throw Erro("O local \"" + nomeLocal + "\" nao foi encontrado no vetor de locais da Protecao Civil, no construtor de ProtecaoCivil.");
+		}
 
 		// obter num. de socorristas
 		dashIndex = line.find_first_of('/');
@@ -46,14 +77,14 @@ void ProtecaoCivil::openFiles(){
 			dashIndex = line.find_first_of('/');
 			tipoVeiculo = line.substr(0,dashIndex);
 
-			postos.push_back(new Policia(local,numSocorristas,numVeiculos,tipoVeiculo));
+			postos.push_back(new Policia(&locais.at(indexLocal),numSocorristas,numVeiculos,tipoVeiculo));
 		}
 		else if(tipoPosto == "Inem"){
 			// obter tipo de veiculo
 			dashIndex = line.find_first_of('/');
 			tipoVeiculo = line.substr(0,dashIndex);
 
-			postos.push_back(new Inem(local,numSocorristas,numVeiculos,tipoVeiculo));
+			postos.push_back(new Inem(&locais.at(indexLocal),numSocorristas,numVeiculos,tipoVeiculo));
 		}
 		else {		// tipoPosto = Bombeiros
 			// obter num. de autotanques
@@ -65,9 +96,10 @@ void ProtecaoCivil::openFiles(){
 			dashIndex = line.find_first_of('/');
 			numAmbulancias = std::stoi(line.substr(0,dashIndex));
 
-			postos.push_back(new Bombeiros(local,numSocorristas,numAutotanques,numAmbulancias));
+			postos.push_back(new Bombeiros(&locais.at(indexLocal),numSocorristas,numAutotanques,numAmbulancias));
 		}
 	}
+	istr.close();
 
 	//////////////////////////////////
 	// TODO: Ler Ficheiro Acidentes //
@@ -87,5 +119,15 @@ ProtecaoCivil::~ProtecaoCivil() {
 
 	// gravar ocorrencias
 	// TODO Call gravar()
+}
+
+unsigned int ProtecaoCivil::findLocal(const std::string &nomeLocal) const{
+	for(unsigned int i=0 ; i<locais.size() ; i++){
+		if(locais.at(i).getNome() == nomeLocal){
+			return i;
+		}
+	}
+
+	return -1;	// local nao foi encontrado
 }
 
