92b049b2128bb9aa4a36efcfdef37280c1957a74
Rui Alves <up201606746@fe.up.pt>
2017-10-17 19:53:05 +0100

    Implemented Date constructor

--- Source/Date.cpp
+++ Source/Date.cpp
@@ -1,7 +1,9 @@
 #include "Date.h"
 
 Date::Date(const std::string &date) {
-
+	dia = std::stoi(date.substr(0,2));
+	mes = std::stoi(date.substr(3,2));
+	ano = std::stoi(date.substr(6,4));
 }
 
 unsigned int getDia() const {
@@ -27,6 +29,7 @@ std::string getData() const {
   std::string tempString = std::to_string(dia) + "-" + std::to_string(mes) + "-" + std::to_string(ano);
   return tempString;
 }
+
  bool operator < (const Date &otherDate) const {
    return this->getCompleteData() < otherDate.getCompleteData();
  }
