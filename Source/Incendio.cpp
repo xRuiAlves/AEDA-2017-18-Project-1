e7eeadf934c0a7868a52ae1c24a020032bd34b30
xRuiAlves <up201606746@fe.up.pt>
2017-10-17 20:19:45 +0100

    Removed printInfoAcidente() from Incendio Class - Pure Virtual functions CAN NOT be implemented in the classe

--- Source/Incendio.cpp
+++ Source/Incendio.cpp
@@ -14,9 +14,3 @@ const unsigned int Incendio::getNumAutotanquesNecess() const{
 Incendio::~Incendio() {
 	// TODO Auto-generated destructor stub
 }
-
-void Incendio::printInfoAcidente() const {
-	std::cout << "***  INCENDIO  ***" << std::endl;
-	std::cout << "Número de carros necessários: " << numCarrosNecess << std::endl;
-	std::cout << "Número de autotanques necessários: " << numAutotanquesNecess << std::endl;
-}
