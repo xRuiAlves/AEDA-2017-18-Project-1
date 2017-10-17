78a92315b2001ed42337271ccc6b87a6f71d8120
Rui Alves <up201606746@fe.up.pt>
2017-10-17 19:59:41 +0100

    Fixed error in getData() -> Wasn't returning in DD-MM-AAAA format

--- Source/Date.cpp
+++ Source/Date.cpp
@@ -1,6 +1,6 @@
 #include "Date.h"
 
-Date::Date(const std::string &date) {
+Date::Date(const std::string &date) {		// formato DD-MM-AAAA
 	dia = std::stoi(date.substr(0,2));
 	mes = std::stoi(date.substr(3,2));
 	ano = std::stoi(date.substr(6,4));
@@ -26,8 +26,31 @@ unsigned int getCompleteData() const {
 }
 
 std::string getData() const {
-  std::string tempString = std::to_string(dia) + "-" + std::to_string(mes) + "-" + std::to_string(ano);
-  return tempString;
+	std::string dia, mes, ano;
+	
+	// Obter dia
+	if (dia < 10)			// tem apenas um digito
+		dia = "0" + std::stoi(dia);
+	else 
+		dia = std::stoi(dia);
+	
+	// Obter mes
+	if (mes < 10)			// tem apenas um digito
+		mes = "0" + std::stoi(mes);
+	else
+		mes = std::stoi(mes);
+	
+	// Obter ano
+	if (ano < 10)			// tem apenas um digito
+		ano = "000" + std::stoi(ano);
+	else if (ano < 100)		// tem apenas dois digitos
+		ano = "00" + std::stoi(ano);
+	else if (ano < 1000)		// tem apenas tres digitos
+		ano = "0" + std::stoi(ano);
+	else
+		ano = std::stoi(ano);
+	
+	return (dia + mes + ano);
 }
 
  bool operator < (const Date &otherDate) const {
