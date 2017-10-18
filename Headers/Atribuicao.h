10112c606d45f3851f3576ae6678b945a363749c
Whiskas123 <joaobernardonarciso@gmail.com>
2017-10-18 12:06:16 +0100

    Added Class Atribuicao

---
+++ Headers/Atribuicao.h
@@ -0,0 +1,31 @@
+#ifndef ATRIBUICAO_H_
+#define ATRIBUICAO_H_
+#include <string>
+#include "Posto.h"
+
+class Atribuicao {
+private:
+	const Posto* posto ;
+	const unsigned int numSocorristas;
+	const unsigned int numVeiculos;
+	const std::string tipoVeiculos;
+public:
+	const unsigned int getPosto() const;
+	const unsigned int getNumSocorristas() const;
+	const unsigned int getNumVeiculos() const;
+	const std::string getTipoVeiculos() const;
+
+
+};
+
+
+
+
+
+/*
+ * Posto*
+ * num  socor
+ * numveoi
+ * string tipo de veic
+ *TODOS PODE SWER COST
+ */
---
+++ Source/Atribuicao.cpp
@@ -0,0 +1,15 @@
+#include "Atribuicao.h"
+
+const unsigned int getPosto() const{
+	return posto;
+}
+const unsigned int getNumSocorristas() const{
+	return numSocorristas;
+}
+const unsigned int getNumVeiculos() const{
+	return numVeiculos;
+}
+const std::string getTipoVeiculos() const{
+	return tipoVeiculos;
+}
+
