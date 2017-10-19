ca5fdc271c645ae72ab1125286445c046e2f0f97
xRuiAlves <up201606746@fe.up.pt>
2017-10-19 22:35:48 +0100

    Created Doxygen Documentation for Date

--- Headers/Date.h
+++ Headers/Date.h
@@ -2,19 +2,61 @@
 #define DATE_H_
 #include <string>
 
+/**
+ * Classe Data utilizada para comparar datas de ocorrencias.
+ */
 class Date {
 private:
-	unsigned int dia;
-	unsigned int mes;
-	unsigned int ano;
+	unsigned int dia;	/**< Dia referente à data. */
+	unsigned int mes;	/**< Mês referente à data. */
+	unsigned int ano;	/**< Ano referente à data. */
 public:
+	/**
+	 * @brief Construtor da classe Date.
+	 * @param date - string que representa a data no formato DD-MM-AAAA.
+	 */
 	Date(const std::string &date);
+
+	/**
+	 * @brief Permite obter o dia da data.
+	 * @return Retorna o dia da data.
+	 */
 	unsigned int getDia() const;
+
+	/**
+	 * @brief Permite obter o mês data.
+	 * @return Retorna o mês da data.
+	 */
 	unsigned int getMes() const;
+
+	/**
+	 * @brief Permite obter o ano da data.
+	 * @return Retorna o ano da data.
+	 */
 	unsigned int getAno() const;
+
+	/**
+	 * @brief Permite obter a data num formato útil para comparar datas.
+	 * @return Retorna a data em formato numérico AAAAMMDD, útil para comparar datas diretamente.
+	 */
 	unsigned int getCompleteData() const;
-	std::string getData() const;	// retorna string DD-MM-AAAA
+
+	/**
+	 * @brief Permite obter a data num formato agradável para o utilizador.
+	 * @return Retorna a data no formato DD-MM-AAAA.
+	 */
+	std::string getData() const;
+
+	/**
+	 * @brief Operador < para comparar datas.
+	 * @param Recebe uma outra data para comparar à data em questão.
+	 * @return Retorna true caso a data em questão seja anterior à data a comparar, false caso contrário.
+	 */
 	bool operator<(const Date &otherDate) const;
+
+	/**
+	 * @brief Destrutor da classe Date.
+	 */
 	~Date();
 };
 
