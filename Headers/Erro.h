1a80c7567aaedd200efb82e9dcc4c4ba44f55122
xRuiAlves <up201606746@fe.up.pt>
2017-10-19 22:27:19 +0100

    Created Doxygen Documentation for Erro

--- Headers/Erro.h
+++ Headers/Erro.h
@@ -2,10 +2,22 @@
 #define ERRO_H_
 #include <string>
 
+/**
+ * Classe Erro utilizada para tratamento de exceções no decorrer do programa.
+ */
 class Erro {
-	const std::string info;
+	const std::string info;		/**< Informação do erro que ocorreu aquando uma exceção */
 public:
+	/**
+	 * @brief Construtor da classe Erro
+	 * @param info - Descrição do tipo de erro que aconteceu aquando o lancamento de uma exceção
+	 */
 	Erro(const std::string &info) : info(info) {}
+
+	/**
+	 * @brief Permite obter a descrição do Erro
+	 * @return Retorna a descrição do Erro
+	 */
 	const std::string getInfo() const { return info; }
 };
 
