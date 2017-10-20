7176d0805fd6409b0baf92ea4ccb32175741b419
xRuiAlves <up201606746@fe.up.pt>
2017-10-20 09:40:28 +0100

    Created Doxygen Documentation for IncendioDomestico

--- Headers/IncendioDomestico.h
+++ Headers/IncendioDomestico.h
@@ -3,14 +3,44 @@
 #include <string>
 #include "Incendio.h"
 
+/**
+ * Incêndio do tipo Doméstico. Classe derivada da classe Incêndio.
+ */
 class IncendioDomestico : public Incendio {
 private:
-	const std::string tipoCasa;
+	const std::string tipoCasa;			/**< Tipo de casa em que tem lugar a ocorrência. Pode ser "Apartamento" ou "Moradia". */
 public:
+	/**
+	 * @brief Construtor da classe IncendioDomestico
+	 * @param data - Data da ocorrência no formato DD-MM-AAAA
+	 * @param local - Apontador para o local onde teve lugar o acidente
+	 * @param numOcorrencia - Numero atribuído à ocorrência
+	 * @param numBombeirosNecess - Número de bombeiros necessários para tratar da ocorrência
+	 * @param numAutotanquesNecess - Número de autotanques necessários para tratar da ocorrência
+	 * @param tipoCasa - Tipo de casa em que tem lugar a ocorrência
+	 */
 	IncendioDomestico(const std::string &data, const Local* local, unsigned int numOcorrencia, unsigned int numBombeirosNecess, unsigned int numAutotanquesNecess, const std::string &tipoCasa);
+
+	/**
+	 * @brief Destrutor da classe IncendioDomestico
+	 */
 	~IncendioDomestico();
+
+	/**
+	 * @brief Permite obter o tipo de casa em que tem lugar a ocorrência. Pode ser "Apartamento" ou "Moradia"
+	 * @return Retorna "Apartamento" ou "Moradia", consoante o tipo de casa.
+	 */
 	const std::string getTipoCasa() const;
+
+	/**
+	 * @brief Permite obter o tipo de incêndio em questão.
+	 * @return Retorna "Incendio Domestico"
+	 */
 	std::string getTipoAcidente() const;
+
+	/**
+	 * @brief Imprime no ecrã toda a informação relativa a este Incêndio Doméstico.
+	 */
 	void printInfoAcidente() const;
 };
 
