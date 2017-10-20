a779d9143ad214a13eceda8feae99d02d8385f86
xRuiAlves <up201606746@fe.up.pt>
2017-10-20 09:36:36 +0100

    Created Doxygen Documentation for Atribuicao

--- Headers/Atribuicao.h
+++ Headers/Atribuicao.h
@@ -3,20 +3,53 @@
 #include <string>
 #include "Posto.h"
 
+/**
+ * Representa uma atribuição de meios de um posto para um acidente.
+ */
 class Atribuicao {
 private:
-	const Posto* posto ;
-	const unsigned int numSocorristas;
-	const unsigned int numVeiculos;
-	const std::string tipoVeiculos;
+	const Posto* posto ;					/**< Apontador para o posto de onde originam os meios de apoio 	*/
+	const unsigned int numSocorristas;		/**< Número de socorristas entrevenientes na atribuição			*/
+	const unsigned int numVeiculos;			/**< Número de veículos entrevenientes na atribuição			*/
+	const std::string tipoVeiculos;			/**< Tipo de veículos entrevenientes na atribuição				*/
 public:
+	/**
+	 * @brief Construtor da classe Atribuicao
+	 * @param posto - Apontador para o posto de onde originam os meios de apoio
+	 * @param numSocorristas - Número de socorristas entrevenientes na atribuição
+	 * @param numVeiculos - Número de veículos entrevenientes na atribuição
+	 * @param tipo Veiculos - Tipo de veículos entrevenientes na atribuição
+	 */
 	Atribuicao(const Posto* posto, const unsigned int numSocorristas, const unsigned int numVeiculos, const std::string &tipoVeiculos);
+
+	/**
+	 * @brief Destrutor da classe Atribuicao
+	 */
+	~Atribuicao();
+
+	/**
+	 * @brief Permite obter o posto de onde originam os meios de apoio
+	 * @return Retorna o apontador para o posto de onde originam os meios de apoio
+	 */
 	const Posto* getPosto() const;
+
+	/**
+	 * @brief Permite obter o número de socorristas entrevenientes na atribuição
+	 * @return Retorna o número de socorristas entrevenientes na atribuição
+	 */
 	const unsigned int getNumSocorristas() const;
+
+	/**
+	 * @brief Permite obter o número de veículos entrevenientes na atribuição
+	 * @return Retorna o número de veículos entrevenientes na atribuição
+	 */
 	const unsigned int getNumVeiculos() const;
+
+	/**
+	 * @brief Permite obter o tipo de veículos entrevenientes na atribuição
+	 * @return Retorna o tipo de veículos entrevenientes na atribuição
+	 */
 	const std::string getTipoVeiculos() const;
-
-
 };
 
 
